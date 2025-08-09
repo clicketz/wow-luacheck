195cc77159c213e09de018cf191cbbc60cfa9c48
clicketz <40314230+clicketz@users.noreply.github.com>
2025-08-09 19:58:46 +0000

    Update custom_globals.lua

--- custom_globals.lua
+++ custom_globals.lua
@@ -1,1 +1,16 @@
+-- Add your own custom global variables here.
+-- The script will automatically merge these with the fetched globals.
+-- Use the format "MyGlobal" one per line.
 
+-- My AddOns
+"BuffOverlay"
+"BuffOverlayDB"
+"BuffOverlayBorderTemplateMixin"
+"WhisperCraft"
+"WhisperCraftDB"
+
+-- Third Party AddOns
+"ElvUF_Parent" -- ElvUI
+"GAME_LOCALE"
+"LibStub"
+"Scorpio"
